a91338f257b2fe495f3341ba637c14471266a60e
tarunb12 <tbelani@ufl.edu>
2019-04-30 00:21:53 -0400

    exceptions to error matches

--- src/main.ml
+++ src/main.ml
@@ -7,7 +7,8 @@ open Codegen ;;
 let filename : string = Sys.argv.(1) ;;
 let out_file : string = Sys.argv.(2) ;;
 
-let produce_error (exn : exn) : unit = string_of_exception filename exn
+let produce_error (exn : exn) : unit = exn
+  |> string_of_exception filename
   |> error_program
   |> codegen_ast
   |> print_module out_file ;;
